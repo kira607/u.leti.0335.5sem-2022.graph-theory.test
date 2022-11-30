82b0a79269812bf0de9f807ada8b04f37a9102a4
Kirill <kirill.leskin@partner.manga.com>
2022-12-01 00:08:17 +0100

    add getting graph as latex tikz pic

--- scripts/graph/helpers.py
+++ scripts/graph/helpers.py
@@ -1,11 +1,13 @@
 from typing import Any, List, Set, Iterable
 
+from dot2tex import dot2tex
+
 from .graph import Graph
 from .infinity import inf, InfNum
 
 
-def mkg(vertices: Iterable[str] = (), edges: Iterable[tuple[str, str]] = ()):
-    graph = Graph()
+def mkg(vertices: Iterable[str] = (), edges: Iterable[tuple[str, str]] = (), label: str = 'G'):
+    graph = Graph(label)
     for vertex in vertices:
         graph.add_vertex(vertex)
     for edge in edges:
@@ -15,3 +17,7 @@ def mkg(vertices: Iterable[str] = (), edges: Iterable[tuple[str, str]] = ()):
 
 def get_vertices_names(graph: Graph) -> List[str]:
     return [v.name for v in graph.vertices]
+
+
+def get_latex_tikz_string(graph: Graph):
+    return dot2tex(graph.dot, figonly=True)
